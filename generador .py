20eb2229f59a21a6a35efcda5686b33f4f1caf31
tati9914 <tatikononovas@gmail.com>
2025-01-28 15:52:14 -0300

    Update generador .py

--- generador .py
+++ generador .py
@@ -1,1 +1,8 @@
+import random
 
+caracter=['+','-','/','*','!','&','$','#','?','=','@','a','b','c','d','e','f','g','h','i','j','k','l','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
+lon=int(input('de cuantos caracteres se compondra la contrasena deseada'))
+respuesta=''
+for i in range(lon):
+    respuesta+=random.choice (caracter)
+print(respuesta)
